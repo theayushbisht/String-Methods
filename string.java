7b5abda51172703cd0e582f4581e2a02c6e8b569
Ayush Bisht <148045610+theayushbisht@users.noreply.github.com>
2024-08-12 17:13:57 +0530

    string.java

--- string.java
+++ string.java
@@ -13,3 +13,34 @@ public class string {
 
      }
 }
+public class string {
+    public static void main(String[] args) {
+//        String name = "I am Ayush";
+////        System.out.println(name);
+////        String lstring = name.toUpperCase();
+////        System.out.println(lstring);
+//        name= name.replace(" ","{}");
+//        System.out.println(name);
+
+//        String text = "Dear <|name|> , Thanks a lot!";
+//        text = text.replace("<|name|> ","Ayush");
+//        System.out.println(text);
+
+//        String name = "Ayush";
+//       char st = name.charAt(3);
+//        System.out.println(st);
+
+//        String A1 = "hello world";
+//        String A2 = "Yello world";
+//        System.out.println(A1.compareTo(A2));
+
+        String A1 = "hello Java";
+        String A2 = "World";
+        String A3 = A1.concat(A2);
+//        System.out.println(A3);
+//        System.out.println(A1.contains("hello"));
+//        System.out.println(A1.endsWith("0"));
+//        System.out.println(A1.equals(A2));
+        System.out.println(A1.trim()+"World");
+     }
+}
