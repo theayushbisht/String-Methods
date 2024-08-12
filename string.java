de3889225ac243969655ef2e24562572253bda87
Ayush Bisht <148045610+theayushbisht@users.noreply.github.com>
2024-08-12 17:14:23 +0530

    string.java

--- string.java
+++ string.java
@@ -7,21 +7,6 @@ public class string {
 //        name= name.replace(" ","{}");
 //        System.out.println(name);
 
-        String text = "Dear <|name|> , Thanks a lot!";
-        text = text.replace("<|name|> ","Ayush");
-        System.out.println(text);
-
-     }
-}
-public class string {
-    public static void main(String[] args) {
-//        String name = "I am Ayush";
-////        System.out.println(name);
-////        String lstring = name.toUpperCase();
-////        System.out.println(lstring);
-//        name= name.replace(" ","{}");
-//        System.out.println(name);
-
 //        String text = "Dear <|name|> , Thanks a lot!";
 //        text = text.replace("<|name|> ","Ayush");
 //        System.out.println(text);
